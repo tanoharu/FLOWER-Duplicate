bd048e1b0327bf617192f7c33513b27fd204432d
hak <iixwxii1212@gmail.com>
2021-02-24 10:15:53 +0900

    FLOWER-copy_test1

---
+++ js/jqurey.js
@@ -0,0 +1,6 @@
+$(function() {
+		$('.l-header>img').addClass('done');
+		$('.l-header>h1').addClass('done');
+		$('.c-fixbar-img>a>img').addClass('done');
+		$('.c-fixbar>p').addClass('done');
+});
