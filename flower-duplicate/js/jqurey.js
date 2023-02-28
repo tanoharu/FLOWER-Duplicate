3f2da23c2f31f8178a0c3f8290d366567a901f76
tanoharu <dbtnhr@gmail.com>
2023-02-28 21:52:27 +0900

    Delete flower-duplicate directory

--- flower-duplicate/js/jqurey.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-		$('.l-header>img').addClass('done');
-		$('.l-header>h1').addClass('done');
-		$('.c-fixbar-img>a>img').addClass('done');
-		$('.c-fixbar>p').addClass('done');
-});
